a25425dc95625a1373d53f2f4c8af932f8f00fa9
Peter Kitonga <petkitonga@gmail.com>
2017-05-12 13:25:50 +0300

    Add project files.

---
+++ Cubic ERP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cubic_ERP.MvcApplication" Language="C#" %>
---
+++ Cubic ERP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cubic_ERP.Startup))]
+namespace Cubic_ERP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
