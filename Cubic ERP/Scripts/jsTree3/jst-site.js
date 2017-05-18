0fdd0b339cb4ea483f8fd18e7b0dbbe923b70de0
Peter Kitonga <petkitonga@gmail.com>
2017-05-18 20:34:22 +0300

    Setting up the main menu using jsTree

--- Cubic ERP/Scripts/jsTree3/jst-site.js
+++ Cubic ERP/Scripts/jsTree3/jst-site.js
@@ -4,12 +4,39 @@
             'core': {
                 "themes": {
                     "name": "default",
-                    "dots": true,
+                    "dots": false,
                     "icons": true
                 }
-            }
+            },
+
+            "types": {
+                "default": {
+                    "icon": "glyphicon glyphicon-folder-open"
+                },
+                "demo": {
+                    "icon": "glyphicon glyphicon-file"
+                }
+            },
+
+            "search": {
+
+                "case_insensitive": true,
+                "show_only_matches": true
+                
+            },
+
+            "plugins": ["types", "search"]
         });
 
-       
+        $('#js-tree').jstree("open_all");
+
+        var to = false;
+        $('#search-input').keyup(function () {
+            if (to) { clearTimeout(to); }
+            to = setTimeout(function () {
+                var v = $('#search-input').val();
+                $('#js-tree').jstree('search', v);
+            }, 250);
+        });
     }
 );
